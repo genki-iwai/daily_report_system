39ae85e4f871ddd6afbc4eed4727c43a712ffe5f
genki.iwai <vo_genki@icloud.com>
2021-02-17 20:49:16 +0900

    Add Employee edit

--- src/controllers/employees/EmployeesCreateServlet.java
+++ src/controllers/employees/EmployeesCreateServlet.java
@@ -71,7 +71,7 @@ public class EmployeesCreateServlet extends HttpServlet {
                 em.getTransaction().begin();
                 em.persist(e);
                 em.getTransaction().commit();
-                request.getSession().setAttribute("flush", "登録が完了しました");
+                request.getSession().setAttribute("flush", "登録が完了しました。");
                 em.close();
 
                 response.sendRedirect(request.getContextPath() + "/employees/index");
